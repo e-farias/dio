8994660602dcb2747c74823bb2b6243852c93db8
e-farias <farias.softwares@gmail.com>
2025-07-11 02:44:16 -0300

    challenge: add isValid validation

---
+++ trilha-react-desafio-4/src/components/Button/index.tsx
@@ -0,0 +1,10 @@
+import { ButtonContainer } from "./styles";
+import { IButtonProps } from "./types";
+
+const Button = ({ title, ...rest }: IButtonProps) => {
+  return (
+    <ButtonContainer {...rest}>{title}</ButtonContainer>
+  );
+};
+
+export default Button;
